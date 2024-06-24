9204dfc1ea24c976b333cc8153cbf55adeb89d02
Ruttaphong Worachartudomphong <ruttaphong.w@vananava.com>
2024-06-24 08:20:22 +0700

    update ui

--- resources/views/livewire/user/home.blade.php
+++ resources/views/livewire/user/home.blade.php
@@ -1,9 +1,14 @@
-<div class="grid gap-2 min-h-svh">
-    <h1 class="text-center text-2xl">คุณเป็น <b>‘คนช่างเลือก’</b> <br>แบบไหน ?</h1>
-    {{-- <x-button label="next" :href="route('ClientProfile')"/> --}}
-    <a href="{{route('ClientProfile')}}" 
-        class="bg-yellow-600 rounded-full shadow-inner px-10 py-4 text-center w-min m-auto"> 
-        คลิก 
-    </a>
+<div class="grid gap-2 min-h-svh overflow-scroll h-full bg-[#FFF2DC] px-4 xs:px-1">
+    <div style="background: url({{ asset('img/Q0-1.png') }}) center bottom; background-size:contain; background-repeat: no-repeat;" 
+    class="relative">
+        <h1 class="text-center text-2xl pt-24">คุณเป็น <b>‘คนช่างเลือก’</b> <br>แบบไหน ?</h1>
+        {{-- <x-button label="next" :href="route('ClientProfile')"/> --}}
+        <div class="absolute bottom-28 w-full">
+        <a href="{{route('ClientProfile')}}" 
+            class="bg-yellow-600 rounded-full block m-auto shadow-inner px-10 py-4 text-center w-min m-auto"> 
+            คลิก 
+        </a>
+        </div>
+    </div>
     {{-- Nothing in the world is as soft and yielding as water. --}}
 </div>
--- resources/views/livewire/user/profile.blade.php
+++ resources/views/livewire/user/profile.blade.php
@@ -10,16 +10,26 @@
         @break
 
     @default
-    <div class="grid gap-2 min-h-svh h-full bg-purple-50">
+    <div class="grid gap-2 min-h-svh h-full">
+        <div style="background: url({{ asset('img/Q0-2.png') }}) center bottom; background-size:contain; background-repeat: no-repeat;" 
+    class="relative">
         <form wire:submit.prevent="goQuiz" class="grid gap-2 min-h-svh h-full">
-        <x-input label="name" wire:model="name" type="text" min="1" max="20"/>
-        <x-input label="age" wire:model="age" type="number" min="1" max="99"/>
-        <x-button type="submit" label="let go" wire:click="goQuiz"/>
-        <a href="{{route('ClientProfile')}}" 
+        <div class="absolute top-[50%] w-full px-[20%]">
+            <x-input label="name" wire:model="name" type="text" min="1" max="20"/>
+        </div>
+        <div class="absolute top-[62%] w-full px-[20%]">
+            <x-input label="age" wire:model="age" type="number" min="1" max="99"/>
+        </div>
+        <div class="absolute top-[78%] w-full px-[20%] text-center">
+        <x-button class="m-auto" type="submit" label="let go" wire:click="goQuiz"/>
+        </div>
+        {{-- <a wire:click="goQuiz"
             class="bg-yellow-600 rounded-full shadow-inner px-10 py-4 text-center w-min m-auto"> 
             เริ่มกันเลย 
-        </a>
+        </a> --}}
         </form>
+
+        </div>
         {{-- Nothing in the world is as soft and yielding as water. --}}
     </div>
         
