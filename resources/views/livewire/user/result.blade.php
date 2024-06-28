a4f69b6c751cc5746811c2836e5e370a746b7275
Ruttaphong Worachartudomphong <ruttaphong.w@vananava.com>
2024-06-28 12:25:30 +0700

    update result

--- resources/views/livewire/user/result.blade.php
+++ resources/views/livewire/user/result.blade.php
@@ -1,10 +1,9 @@
-<div class="relative font-db_heaventrounded h-screen">
+<div class="relative font-db_heaventrounded">
     <div id="result" >
         <img src="{{asset('img/result/'.$result.'.jpg')}}">
         <h2 class="absolute top-[13%] left-[45%] w-1/2 text-center text-3xl text-primary-100">{{$name??""}}</h2>
     </div>
 
-    <canvas id="myCanvas" class="w-full hidden "></canvas>
     <div id="btn" class="absolute bottom-[6%] left-[60%] w-[30%] h-[12%] grid gap-6">
         <img class="cursor-pointer" src="{{asset('img/result_btn/button_Ans-A_Save-01.png')}}" onclick="saveImg()">
         <a rel="nofollow noopener noreferrer"
@@ -18,8 +17,9 @@
         
     </div>
     
+    <canvas id="myCanvas" class="w-full hidden"></canvas>
     <script>
-        let loadImageOnCanvasAndResizeCanvasToFitImage = (canvas, imageUrl,text) => {
+        let loadImageOnCanvasAndResizeCanvasToFitImage = async(canvas, imageUrl,text) => {
             // Get the 2D Context from the canvas
             let ctx = canvas.getContext("2d");
 
@@ -35,39 +35,45 @@
                 // Set the canvas to the same size as the image.
                 canvas.width = loadedImageWidth;
                 canvas.height = loadedImageHeight;
-                let font = FontFace('myFont',"url('/resources/fonts/DB\ HeaventRounded\ Bd\ v3.2.1.ttf')")
-                await font.load()
-                document.fonts.add(font)
-
-                ctx.font = "220px myFont"
+				console.log("load canvas");
+				
+                //let font = new FontFace('myFont',"url('//fonts/DB\ HeaventRounded\ Bd\ v3.2.1.ttf')")
+                //await font.load()
+                //document.fonts.add(font)
+				console.log("load font");
+                ctx.font = "220px DB_HeaventRounded"
                 ctx.fillStyle = "#8b4d36";
                 ctx.textAlign = "center";
                 ctx.textBaseline = "top";
                 // Draw the image on to the canvas.
                 ctx.drawImage(img, 0, 0);
                 ctx.fillText('{{$name??"-"}}', 1450, 500);
+				return ctx
             };
-
-            // Now that we have set up the image "onload" handeler, we can assign
+			
+  			//const win = window.open(dataUrl, '_blank');
+            
+			// Now that we have set up the image "onload" handeler, we can assign
             // an image URL to the image.
             img.src = imageUrl;
             // var a = document.createElement('a');
             // a.href = imageUrl;
             // a.download = 'result.jpg';
-            // // console.log('create element'+'a')
+             // console.log('create element'+'a')
             // a.click(); 
+			
         };
 
         // Run code after the page is loaded
         document.addEventListener("DOMContentLoaded", () => {
         // Setting up the canvas
-            // let theCanvas = document.getElementById("myCanvas");
+            let theCanvas = document.getElementById("myCanvas");
 
             // Some image URL..
-            // let imageUrl ="{{asset('img/result/'.$result.'.jpg')}}";
+            let imageUrl ="{{asset('img/result/'.$result.'.jpg')}}";
 
             // Load image on the canvas & re-size the canvas based on the image size
-            // loadImageOnCanvasAndResizeCanvasToFitImage(theCanvas, imageUrl,"{{$name??"-"}}");
+            loadImageOnCanvasAndResizeCanvasToFitImage(theCanvas, imageUrl,"{{$name??"-"}}");
         
         });
 
@@ -75,9 +81,10 @@
         function saveImg(){
             let theCanvas = document.getElementById("myCanvas");
             // Some image URL..
-            let imageUrl ="{{asset('img/result/'.$result.'.jpg')}}";
-            // Load image on the canvas & re-size the canvas based on the image size
-            loadImageOnCanvasAndResizeCanvasToFitImage(theCanvas, imageUrl,"{{$name??"-"}}");
+			theCanvas.classList.remove("hidden")
+			let myWindows = window.open('',"_blank")
+			myWindows.document.body.appendChild(theCanvas)
+			theCanvas.classList.add("hidden")
         }
 
     </script>
